c48b1709410a815509463644214fb1f39a596709
rmitsui0522 <82199182+rmitsui0522@users.noreply.github.com>
2022-07-01 15:34:14 +0900

    add: auth
    - module
    - service
    - passport.local-strategy

---
+++ app/src/auth/auth.service.spec.ts
@@ -0,0 +1,42 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { UserEntity } from '../users/entities/user.entity';
+import {
+  MockType,
+  repositoryMockFactory,
+} from '../test-util/repositoryMockFactory';
+import { user } from '../test-util/users.seed';
+
+describe('AuthService', () => {
+  let service: AuthService;
+  let repository: MockType<Repository<UserEntity>>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useFactory: repositoryMockFactory,
+        },
+      ],
+    }).compile();
+
+    service = module.get<AuthService>(AuthService);
+    repository = module.get(getRepositoryToken(UserEntity));
+  });
+
+  it('should be defined', async () => {
+    const { password, ...others } = user;
+
+    repository.findOne.mockReturnValue(user);
+
+    expect(await service.validateUser(user.userId, user.password)).toEqual(
+      others,
+    );
+  });
+});
---
+++ app/src/test-util/users.seed.ts
@@ -0,0 +1,9 @@
+import { UserEntity } from '../users/entities/user.entity';
+
+export const user: UserEntity = {
+  id: 1,
+  userId: 'testuser',
+  nickname: 'test',
+  email: 'test@user.com',
+  password: 'test',
+};
