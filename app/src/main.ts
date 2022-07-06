381784dc7ca534881cd569015a2cbc08b4a9bd17
rmitsui0522 <82199182+rmitsui0522@users.noreply.github.com>
2022-07-06 20:14:22 +0900

    update: main.ts
    add packages
    - helmet
    - express-session
    - cookie-parser

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.use(cookieParser());
+  // TODO: config.moduleの実装
+  app.use(
+    session({
+      secret: 'SESSION_SECRET',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
