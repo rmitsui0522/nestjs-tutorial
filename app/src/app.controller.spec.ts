b53558ad827c02f8b98b2b63ba6516b824d3bcec
rmitsui0522 <82199182+rmitsui0522@users.noreply.github.com>
2022-07-06 20:11:22 +0900

    update: app.controller

--- app/src/app.controller.spec.ts
+++ app/src/app.controller.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { UsersService } from './users/users.service';
+import { UsersFactory } from './users/factory/users.factory';
+import { UserEntity } from './users/entities/user.entity';
+import { repositoryMockFactory } from './test-util/repositoryMockFactory';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +14,24 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AuthService,
+        JwtService,
+        UsersService,
+        UsersFactory,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useFactory: repositoryMockFactory,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should be defined"', () => {
+      expect(appController).toBeDefined();
     });
   });
 });
--- app/src/app.controller.ts
+++ app/src/app.controller.ts
@@ -1,21 +1,7 @@
-import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Controller } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
 }
