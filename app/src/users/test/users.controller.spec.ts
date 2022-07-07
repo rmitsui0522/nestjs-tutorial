2bb91251eab8f3e6b6d591254266ef9ea20fc596
rmitsui0522 <82199182+rmitsui0522@users.noreply.github.com>
2022-07-07 10:48:33 +0900

    update: file moved

--- app/src/users/test/users.controller.spec.ts
+++ app/src/users/test/users.controller.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { UserEntity } from './entities/user.entity';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
-import { UsersFactory } from './factory/users.factory';
+import { UserEntity } from '../entities/user.entity';
+import { UsersController } from '../users.controller';
+import { UsersService } from '../users.service';
+import { UsersFactory } from '../factory/users.factory';
 import {
   MockType,
   repositoryMockFactory,
-} from '../test-util/repositoryMockFactory';
-import { user } from '../test-util/users.seed';
+} from '../../test-util/repositoryMockFactory';
+import { user } from '../../test-util/users.seed';
 
 describe('UsersController', () => {
   let controller: UsersController;
